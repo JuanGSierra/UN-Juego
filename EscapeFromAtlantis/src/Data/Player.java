cd13157e53e7af640ccc2b1e49d7c44c48b9db29
JuanGSierra <jgsierraa@unal.edu.co>
2016-11-26 18:54:43 -0500

    stufff

--- EscapeFromAtlantis/src/Data/Player.java
+++ EscapeFromAtlantis/src/Data/Player.java
@@ -16,25 +16,25 @@ public class Player implements Serializable {
     private static int numberOfPlayers = 0;
     private String name;
     private Villager[] villagers;
-    private int id=0;
+    private int id = 0;
 
-    public Player() {
-        
+    public Player(String name) {
+
         this.name = name;
         villagers = new Villager[10];
-        villagers[0] = new Villager(1,id);
-        villagers[1] = new Villager(1,id);
-        villagers[2] = new Villager(1,id);
-        villagers[3] = new Villager(2,id);
-        villagers[4] = new Villager(2,id);
-        villagers[5] = new Villager(3,id);
-        villagers[6] = new Villager(3,id);
-        villagers[7] = new Villager(4,id);
-        villagers[8] = new Villager(5,id);
-        villagers[9] = new Villager(6,id);
+        villagers[0] = new Villager(1, id);
+        villagers[1] = new Villager(1, id);
+        villagers[2] = new Villager(1, id);
+        villagers[3] = new Villager(2, id);
+        villagers[4] = new Villager(2, id);
+        villagers[5] = new Villager(3, id);
+        villagers[6] = new Villager(3, id);
+        villagers[7] = new Villager(4, id);
+        villagers[8] = new Villager(5, id);
+        villagers[9] = new Villager(6, id);
         numberOfPlayers++;
         id = numberOfPlayers;
-        
+
     }
 
     public String getName() {
