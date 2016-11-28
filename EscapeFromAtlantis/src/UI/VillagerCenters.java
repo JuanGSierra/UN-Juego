14d631814a9a14f5dc9b5135ee2a7cfd6726a610
johanr98 <jsromerod@unal.edu.co>
2016-11-27 22:40:34 -0500

    villager centers

---
+++ EscapeFromAtlantis/src/UI/VillagerCenters.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+/**
+ *
+ * @author johans
+ */
+public class VillagerCenters {
+    
+    PixelPoint centermat[][];
+    
+    public VillagerCenters(){
+        centermat = new PixelPoint[13][25];
+        
+        centermat[0][2]= new PixelPoint(280,36);
+        centermat[0][2]= new PixelPoint(375,36);
+        centermat[0][2]= new PixelPoint(425,36);
+        centermat[0][2]= new PixelPoint(470,36);
+        centermat[0][2]= new PixelPoint(520,36);
+        centermat[0][2]= new PixelPoint(560,36);
+        centermat[0][2]= new PixelPoint(615,36);
+        centermat[0][2]= new PixelPoint(660,36);
+        centermat[0][2]= new PixelPoint(755,36);
+        
+        centermat[0][2]= new PixelPoint(255,80);
+        centermat[0][2]= new PixelPoint(305,80);
+        centermat[0][2]= new PixelPoint(350,80);
+        centermat[0][2]= new PixelPoint(400,80);
+        centermat[0][2]= new PixelPoint(450,80);
+        centermat[0][2]= new PixelPoint(495,80);
+        centermat[0][2]= new PixelPoint(545,80);
+        centermat[0][2]= new PixelPoint(590,80);
+        centermat[0][2]= new PixelPoint(640,80);
+        centermat[0][2]= new PixelPoint(685,80);
+        centermat[0][2]= new PixelPoint(735,80);
+        centermat[0][2]= new PixelPoint(785,80);
+        
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        centermat[0][2]= new PixelPoint(1,1);
+        
+        
+        
+    }
+    
+}
